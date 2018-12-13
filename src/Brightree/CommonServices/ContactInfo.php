6a98564638041b31bc823d05230d980974384e7e
Matthew Kulina <matthewkulina@icloud.com>
2018-12-12 19:42:14 -0500

    Creating contactInfo function

--- src/Brightree/CommonServices/ContactInfo.php
+++ src/Brightree/CommonServices/ContactInfo.php
@@ -3,12 +3,20 @@
 namespace Brightree\CommonServices\SalesOrder;
 
 
-
 class contactInfo {
+  public $PhoneNumber;
+  public $FaxNumber;
+  public $FaxAttention;
+  public $MobilePhoneNumber;
+  public $EmailAddress;
+  public $ExcludeFaxServices;
 
-
-  public function __construct() {
-
+  public function setContactInfo(Contact $contactInfo) {
+    $this->PhoneNumber = $contactInfo;
+    $this->FaxNumber = $contactInfo;
+    $this->FaxAttention = $contactInfo;
+    $this->MobilePhoneNumber = $contactInfo;
+    $this->EmailAddress = $contactInfo;
+    $this->ExcludeFaxServices = $contactInfo;
   }
-
 }

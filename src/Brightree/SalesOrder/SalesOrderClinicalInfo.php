69517127658b00e42c9646dd185e09521c430d02
Matthew Kulina <mkulina@ip-172-31-43-20.ec2.internal>
2018-12-13 09:36:19 -0500

    Created SalesOrder objects

---
+++ src/Brightree/SalesOrder/SalesOrderClinicalInfo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Brightree\SalesOrderClinicalInfo;
+
+
+class SalesOrderClinicalInfo {
+  public $DiagnosisCodes;
+  public $EPSDTCertificationCodeIndicator;
+  public $EPSDTConditionCode;
+  public $MarketingReferral;
+  public $OrderingDoctor;
+  public $Patient;
+  public $RederingProvider;
+  public $SOHeight;
+  public $SOWeight;
+
+}
---
+++ src/Brightree/SalesOrder/SalesOrderGeneralInfo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Brightree\SalesOrderGeneralInfo;
+
+
+class SalesOrderGeneralInfo {
+  public $Branch;
+  public $ClaimNote;
+  public $ClaimNoteTypeKey;
+  public $DateofAdmission;
+  public $DiscountDischarge;
+  public $DiscountPercent;
+  public $ExcludeEligibilityCheck;
+  public $InventoryLocation;
+  public $ManualHold;
+  public $ManualHoldReason;
+  public $PlaceOfService;
+  public $PONumber;
+  public $PrintDate;
+  public $Printed;
+  public $SalesOrderClassification;
+  public $Status;
+  public $StopDate;
+  public $Reference;
+  public $TemplateStatus;
+  public $User1;
+  public $User2;
+  public $User3;
+  public $User4;
+
+}

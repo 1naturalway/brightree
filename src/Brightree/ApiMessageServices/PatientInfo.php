3a4a2808e97b0212cf87888cad3506e285aa2589
Andrew Donald Johnson <ajohnson6494@gmail.com>
2018-12-14 15:42:25 -0500

    fixed naming

--- src/Brightree/ApiMessageServices/PatientInfo.php
+++ src/Brightree/ApiMessageServices/PatientInfo.php
@@ -3,10 +3,8 @@
 namespace Brightree\ApiMessageServices;
 
 use Brightree\CommonServices\ContactInfo;
-use Brightree\ApiMessageServices\LookupValue;
 use Brightree\CommonServices\Name;
 use Brightree\CommonServices\Address;
-use Brightree\CommonServices\ContactInfo;
 
 class PatientInfo {
   public $BrightreeID;
@@ -29,44 +27,9 @@ class PatientInfo {
     $this->ContactInfo = new ContactInfo();
   }
 
-  public function getContactInfo(ContactInfo $contact) {
-    $this->ContactInfo = $contact;
-  }
-
-  public function getMarketingRep(LookupValue $marketingRep) {
-    $this->MarketingRep = $marketingRep;
-  }
-
-  public function getPractitioner(LookupValue $practitioner) {
-    $this->Practitionero = $practitioner;
-  }
-
-  public function getFunctionalAbility(LookupValue $functionalAbility) {
-    $this->FunctionalAbility = $functionalAbility;
-  }
-
-  public function getName(Name $name) {
-    $this->Name = $name;
-  }
-
-  public function getAddress(Address $address) {
-    return $this->Address = $address;
-  }
-
-  public function getContactInfo(ContactInfo $contactInfo) {
-    return $this->ContactInfo = $contactInfo;
-  }
-
-  public function getMarketingRep(LookupValue $lookupValue) {
-    return $this->MarketingRep = $lookupValue;
-  }
-
-  public function getPractitioner(LookupValue $lookupValue) {
-    return $this->Practitioner = $lookupValue;
-  }
-
   public function setGender($Gender) {
-    return $this->Gender = $Gender;
+    $this->Gender = $Gender;
+    return $this;
   }
 
   public function setPatientID($PatientID) {
@@ -87,10 +50,6 @@ class PatientInfo {
   public function setSSN($SSN) {
     $this->SSN = $SSN;
     return $this;
-  }
-
-  public function getFunctionalAbility(LookupValue $lookupValue) {
-    return $this->FunctionalAbility = $lookupValue;
   }
 
   public function setDOB($DOB) {

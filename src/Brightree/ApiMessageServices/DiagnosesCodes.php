07aaa6365198ddff13ff6ef530aac4fa54b71e27
Matthew Kulina <matthewkulina@icloud.com>
2019-01-16 10:34:24 -0500

    Created new Diagnosis Codes class

---
+++ src/Brightree/ApiMessageServices/DiagnosesCodes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brightree\SalesOrder;
+
+class DiagnosisCodes {
+  public $DiagnosisCodes;
+
+
+  public function __construct() {
+    $this->DiagnosisCodes = new ICDCodeInfo();
+
+  }
+
+  public function setDiagnosisCodes(ICDCodeInfo $codes) {
+    $this->DiagnosisCodes = $codes;
+  }
+
+}

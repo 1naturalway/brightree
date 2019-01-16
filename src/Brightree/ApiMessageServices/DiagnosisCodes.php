70fe9f09a114511708ada332872974df2b34aacd
Matthew Kulina <matthewkulina@icloud.com>
2019-01-16 10:55:10 -0500

    Fixing namespacing

--- src/Brightree/ApiMessageServices/DiagnosisCodes.php
+++ src/Brightree/ApiMessageServices/DiagnosisCodes.php
@@ -5,14 +5,11 @@ namespace Brightree\SalesOrder;
 class DiagnosisCodes {
   public $DiagnosisCodes;
 
-
   public function __construct() {
     $this->DiagnosisCodes = new ICDCodeInfo();
-
   }
 
   public function setDiagnosisCodes(ICDCodeInfo $codes) {
     $this->DiagnosisCodes = $codes;
   }
-
 }

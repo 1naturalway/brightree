bf9dbb834082a957a68629f83bec6c89a4e9a191
Bryce Sharrits <bryce.sharrits@1naturalway.com>
2019-02-07 09:50:16 -0500

    Stuff for custom fields.

---
+++ src/Brightree/Services/CustomFieldService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Brightree\Services;
+
+use SoapClient;
+
+class CustomFieldService {
+  public function __constuct($params) {
+    $this->params = $params;
+    $this->wsdl_path = "https://webservices.brightree.net/v0100-1610/CustomFieldService/CustomFieldService.svc?singleWsdl";
+  }
+
+  public function ApiCall($call,$query) {
+    $client = new SoapClient($this->wsdl_path, $this->params);
+    $response = $client->$call($query);
+    return $response;
+  }
+
+  public function CustomFieldValueSaveMultiple($category,$brightreeID,$fieldValues) {
+    return $this->ApiCall('CustomFieldValueSaveMultiple', ['category' => $category, 'brightreeID' => $brightreeID, 'fieldValues' => $fieldValues]);
+  }
+}

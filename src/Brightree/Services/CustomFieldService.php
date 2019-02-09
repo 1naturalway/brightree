cfeaa5c35bc7ae6502b9afd4ce8b5b9049b2478b
Nicholas Cheek <nick@nicholascheek.com>
2019-02-09 11:39:42 -0600

    add trait for apicall

--- src/Brightree/Services/CustomFieldService.php
+++ src/Brightree/Services/CustomFieldService.php
@@ -2,20 +2,15 @@
 
 namespace Brightree\Services;
 
-use SoapClient;
-
 class CustomFieldService {
+
+  use \Brightree\Traits\ApiTrait;
+
   public function __construct($params) {
     $this->params = $params;
     $this->wsdl_path = "https://webservices.brightree.net/v0100-1610/CustomFieldService/CustomFieldService.svc?singleWsdl";
   }
-
-  public function ApiCall($call,$query) {
-    $client = new SoapClient($this->wsdl_path, $this->params);
-    $response = $client->$call($query);
-    return $response;
-  }
-
+  
   public function CustomFieldValueSaveMultiple($category,$brightreeID,$fieldValues) {
     return $this->ApiCall('CustomFieldValueSaveMultiple', ['category' => $category, 'brightreeID' => $brightreeID, 'fieldValues' => $fieldValues]);
   }
--- src/Brightree/Services/DocumentManagmementService.php
+++ src/Brightree/Services/DocumentManagmementService.php
@@ -2,20 +2,16 @@
 
 namespace Brightree\Services;
 
-use SoapClient;
 use Brightree\DocumentManagement\DocumentManagement;
 
 class DocumentManagementService{
-  public function __construct($params) {
-    $this->params = $params;
-    $this->wsdl_path = "https://webservices.brightree.net/v0100-1610/DocumentationService/DocumentManagementService.svc?singleWsdl";
-  }
 
-  public function ApiCall($call,$query) {
-    $client = new SoapClient($this->wsdl_path, $this->params);
-    $response = $client->$call($query);
-    return $response;
-  }
+    use \Brightree\Traits\ApiTrait;
+
+    public function __construct($params) {
+        $this->params = $params;
+        $this->wsdl_path = "https://webservices.brightree.net/v0100-1610/DocumentationService/DocumentManagementService.svc?singleWsdl";
+    }
 
   public function DocumentBatchCreate($BrightreeID) {
     return $this->ApiCall('DocumentBatchCreate', ['BrightreeID' => $BrightreeID]);
@@ -25,23 +21,32 @@ class DocumentManagementService{
     return $this->ApiCall('DocumentBatchSearch', ['BrightreeID' => $BrightreeID]);
   }
 
-  public function DocumentSearch($PtID) {
-    return $this->ApiCall('DocumentSearch', ['searchRequest' => [ 'PatientID' => $PtID ], 'sortRequest' => '']);
+  public function DocumentSearch($PtID) {  
+    return $this->ApiCall('DocumentSearch',
+    	array(
+			'searchRequest' => array(
+				'PatientID' => $PtID
+			),
+			'sortRequest' => ''
+		));
   }
 
-  public function DocumentTypesFetchAll() {
-    return $this->ApiCall('DocumentTypesFetchAll',[]);
+  public function DocumentTypesFetchAll() {  
+    return $this->ApiCall('DocumentTypesFetchAll','');
   }
 
-  public function FetchDocumentContent($DocumentKey) {
+  public function FetchDocumentContent($DocumentKey) {  
     return $this->ApiCall('FetchDocumentContent', ['documentKey' => $DocumentKey]);
   }
 
-  public function GenerateDocumentID($BrightreeID) {
+  public function GenerateDocumentID($BrightreeID) {  
     return $this->ApiCall('GenerateDocumentID', ['DocumentTypeBrightreeID' => $BrightreeID]);
   }
 
-  public function StoreDocument($Contents) {
+  public function StoreDocument($Contents) {  
     return $this->ApiCall('StoreDocument', ['Contents' => $Contents]);
   }
+  
+
+
 }

15d556f6a7187c32f0e028717efbc200b24d6dff
Matthew Kulina <matthewkulina@icloud.com>
2019-04-03 10:22:37 -0400

    Added Document Management Classes

---
+++ src/Brightree/DocumentManagement/DocumentBatch.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brightree\DocumentServices;
+
+use Brightree\ApiMessageServices\LookupValue;
+
+class DocumentBatch {
+  public $BatchDescription;
+  public $BatchName;
+  public $BatchOwner;
+  public $BrightreeID;
+  public $Closed;
+
+  public function __construct() {
+    $this->BatchOwner = new LookupValue();
+  }
+
+  public function setBatchDescription($BatchDescription) {
+    $this->BatchDescription = $BatchDescription;
+    return $this;
+  }
+
+  public function setBatchName($BatchName) {
+    $this->BatchName = $BatchName;
+    return $this;
+  }
+
+  public function setBatchOwner($BatchOwner) {
+    $this->BatchOwner = $BatchOwner;
+    return $this;
+  }
+
+  public function setBrightreeID($BrightreeID) {
+    $this->BrightreeID = $BrightreeID;
+    return $this;
+  }
+
+  public function setClosed($Closed) {
+    $this->Closed = $Closed;
+    return $this;
+  }
+}

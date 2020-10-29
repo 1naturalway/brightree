f84591054f555f6a37f7373563fc5a1f0d821189
Andrew Donald Johnson <ajohnson6494@gmail.com>
2020-10-29 09:35:11 -0400

    Implement changes for DoctorService

--- src/Brightree/ApiMessageServices/Doctor.php
+++ src/Brightree/ApiMessageServices/Doctor.php
@@ -4,13 +4,16 @@ namespace Brightree\ApiMessageServices;
 
 use Brightree\CommonServices\Name;
 use Brightree\ApiMessageServices\MedicalInfo;
+use Brightree\ApiMessageServices\GoScriptsSettings;
 
 class Doctor {
   public $Name;
   public $MedicalInfo;
+  public $GoScriptsSettings;
 
   public function __construct() {
     $this->Name = new Name();
     $this->MedicalInfo = new MedicalInfo();
+    $this->GoScriptsSettings = new GoScriptsSettings();
   }
 }
---
+++ src/Brightree/ApiMessageServices/GoScriptsSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Brightree\ApiMessageServices;
+
+class GoScriptsSettings {
+  public $EnrolledWithGoScripts;
+  public $GoScriptsID;
+
+  public function __construct() {
+  }
+}

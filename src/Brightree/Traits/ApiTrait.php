47fe1894c264f20fcc630486428358ba2c407ce3
Andrew Donald Johnson <ajohnson6494@gmail.com>
2025-07-17 07:22:36 -0400

    Add cleanup for SOAP connections

--- src/Brightree/Traits/ApiTrait.php
+++ src/Brightree/Traits/ApiTrait.php
@@ -8,6 +8,10 @@ trait ApiTrait {
   public function apiCall($call, $query) {
     $client = new SoapClient($this->wsdl_path, $this->params);
     $response = $client->$call($query);
+
+    unset($client); // Closes the SOAP connection
+    gc_collect_cycles(); // Force cleanup in long-running workers
+
     return $response;
   }
 }
